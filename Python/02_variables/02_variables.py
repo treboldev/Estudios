d919b0e048747f3062a100660b65adc2c83e63c0
treboldev <dpvc.chile@gmail.com>
2024-06-16 10:07:02 -0400

    EjemploVar

---
+++ Python/02_variables/02_variables.py
@@ -0,0 +1,22 @@
+# Declaración de variables
+
+nombre = "Juan"
+edad = 30
+pi = 3.14159
+esta_lloviendo = True
+numeros = [1, 2, 3, 4, 5]
+
+# Ejemplo de variables
+
+# Imprimiendo el valor de la variable "nombre"
+print(nombre)
+
+# Calculando la edad en 5 años
+edad_en_5_años = edad + 5
+print(f"Edad en 5 años: {edad_en_5_años}")
+
+# Comprobando si esta lloviendo
+if esta_lloviendo:
+    print("¡Lleva un paraguas!")
+else:
+    print("¡Disfruta del sol!")
