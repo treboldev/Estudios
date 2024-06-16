daa465b21fca40d0e96d1ae1e649d229950d158b
treboldev <dpvc.chile@gmail.com>
2024-06-16 08:43:37 -0400

    UpdateInfo

--- Python/02_tipo_de_datos/02_tipo_datos_ejercicios.py
+++
@@ -1,6 +0,0 @@
-edad = int(input('Ingrese su edad: '))
-altura = float(input('Ingrese su altura: '))
-nombre_completo = str(input('Ingrese su nombre completo: '))
-es_casado = False
-
-print('Hola', nombre_completo, 'segun los datos ingresados mides: ', altura, ' Centimetros y tienes ', edad, 'años, ¿ estas casado?', es_casado)
